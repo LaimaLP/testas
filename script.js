2b7c02c941d79f12f4df98bb16ddda9b4c0e3c2f
LaimaLP <pileliene.laima@gmail.com>
2023-11-29 15:45:27 +0200

    uzd

--- script.js
+++ script.js
@@ -1,36 +1,71 @@
 console.log("test")
 
-const input = '39518025894009348176177748178875936978444418362638085019682007561173738605710808391781432106826309189862218192135292600619080189001380510212854851893689772';
+// const input = '39518025894009348176177748178875936978444418362638085019682007561173738605710808391781432106826309189862218192135292600619080189001380510212854851893689772';
 
 
-function generator(number){
+// function generator(number){
    
-    const arr=[];
-    for(let i = 0; i<number.length; i++){
-        arr.push(number[i]+number[i+1])
-    }
-    let obj = {};
-  for (const pair of arr) {
-    if (obj[pair]) {
-      obj[pair] += 1;
-    } else {
-      obj[pair] = 1;
-    }
-  }
+//     const arr=[];
+
+ 
+//     for(let i = 0; i<number.length-1; i++){
+//         arr.push(number[i]+number[i+1])
+//     }
+
+
+
+//     let obj = {};
+//   for (const pair of arr) {
+//     if (obj[pair]) {
+//       obj[pair] += 1;
+//     } else {
+//       obj[pair] = 1;
+//     }
+//   }
   
-  let maxCount = 0;
-  for (const pair in obj) {
-    if (obj[pair] > maxCount) {
-      maxCount = obj[pair];
-    }
-  }
-    const maxPairs = Object.keys(obj).filter((pair) => obj[pair] === maxCount);
-    return  `Poros: ${maxPairs} kartojasi ${maxCount}`;
-  }
-  console.log(generator(input));
+//   let maxCount = 0;
+//   for (const pair in obj) {
+//     if (obj[pair] > maxCount) {
+//       maxCount = obj[pair];
+//     }
+//   }
+//     const maxPairs = Object.keys(obj).filter((pair) => obj[pair] === maxCount);
+//     return  `Poros: ${maxPairs} kartojasi ${maxCount}`;
+//   }
+//   console.log(generator(input));
 
 
 
 
-generator(input)
+// generator(input)
+
+
+// const poros ={};
+
+// for (let i=0; i<input.length-1; i++){
+//     const nr = unpout[i] + input[i+1];
+//     if(poros[nr]){
+//         poros[nr]++;
+//     }else{
+//         poros[nr]=1
+//     }
+// }
+
+// let maxKiekis = 0;
+// for(const key in poros)
+// {
+//     if(poros[key] >maxKiekis){
+//         maxKiekis=poros[key]
+//     }
+// }
+
+console.log("test")
+
+
+let para="laima"
+
+ console.log(para.slice(0,1).toUpperCase())
+ console.log(para.slice(1,para.length))
+
+
 
